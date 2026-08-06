b6b07e1bb286d324a4144e9dbd19b3e5221f3c8b
axel10 <axel10@163.com>
2026-08-06 10:59:53 +0800

    更换windows系统托盘图标

--- lib/player/platform/desktop_tray_service.dart
+++ lib/player/platform/desktop_tray_service.dart
@@ -81,7 +81,7 @@ class DesktopTrayService with WindowListener {
   Future<void> _initTray() async {
     try {
       debugPrint('[Tray] Initializing system tray...');
-      final iconRelativePath = Platform.isWindows ? 'assets/icons/tray.ico' : 'assets/icons/tray.png';
+      final iconRelativePath = Platform.isWindows ? 'assets/icons/tray_shadow.ico' : 'assets/icons/tray.png';
       final iconAbsolutePath = _getIconAbsolutePath(iconRelativePath);
       debugPrint('[Tray] Absolute icon path: $iconAbsolutePath');
 
